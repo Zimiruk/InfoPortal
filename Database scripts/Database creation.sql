ba48d61fcb355fb8bb0504471f037ac7238594a4
Zimiruk <vadzim.alifiruk@gmail.com>
2020-10-24 13:20:08 +0300

    Added initial database scripts

---
+++ Database scripts/Database creation.sql
@@ -0,0 +1,19 @@
+/*
+More tables will be added
+*/
+
+CREATE DATABASE [InfoPortal]
+
+GO
+
+CREATE TABLE [InfoPortal].[dbo].[Articles]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+	[Name] VARCHAR(50) NOT NULL, 
+	[Theme] VARCHAR(50) NOT NULL, 
+	[AddedOn] DATE NOT NULL DEFAULT GETDATE(),
+	[Language] VARCHAR(50) NOT NULL,
+	[Picture] VARCHAR(100) NOT NULL, 
+	[Video] VARCHAR(100) NOT NULL, 
+	[Link] INT NOT NULL
+)
