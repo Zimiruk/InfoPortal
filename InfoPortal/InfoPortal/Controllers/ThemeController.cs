2417b176fde8ef73ef7a815e8cfaca4b45c344bf
Zimiruk <vadzim.alifiruk@gmail.com>
2020-12-16 23:35:28 +0300

    Added Themes panel

---
+++ InfoPortal/InfoPortal/Controllers/ThemeController.cs
@@ -0,0 +1,29 @@
+﻿using InfoPortal.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace InfoPortal.WebMVC.Controllers
+{
+    public class ThemeController : Controller
+    {
+        public IActionResult Index()
+        {/*
+            var articles = _articleService.GetAll();
+            return View(articles);
+            */
+
+            List<Theme> themes = new List<Theme>
+            {
+                new Theme
+                {
+                    Id = 1,
+                    Name = "Theme"
+                }
+            };
+           
+
+            return View(themes);
+        }
+    }
+}
+ 
